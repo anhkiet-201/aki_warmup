1809cf266c284fa3758105d553bb3712f112b443
Anh Kiệt <igg.anhkiet1@gmail.com>
2026-05-12 13:57:56 +0700

    feat: add PowerShell script for automated warm-up execution

--- runner/run_warmUp.ps1
+++ runner/run_warmUp.ps1
@@ -1,6 +1,9 @@
-﻿# Script chuyên biệt để chạy tính năng Warm Up
+param(
+    [string]$deviceFile
+)
+
+# Script chuyên biệt để chạy tính năng Warm Up
 $PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 # Gọi file lõi điều phối và truyền method
-& "$PSScriptRoot\run_parallel.ps1" -method warmUp
-
+& "$PSScriptRoot\run_parallel.ps1" -method warmUp -deviceFile $deviceFile
